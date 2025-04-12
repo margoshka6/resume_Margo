37fd7493efb8a4238e6e0277088e90d3acfde8f0
margoshka6 <147420760+margoshka6@users.noreply.github.com>
2025-04-12 13:32:13 +0300

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import numpy as np
-from flask import Flask,render_template
+from flask import Flask, render_template
 import os
+
+
 def index():
-    data=pd.read_csv("Ріта.csv")
-    photo="мый.png"
-    name=data[data["category"]=="name"]["text"].values[0]
-    position=data[data["category"]=="position"]["text"].values[0]
-    contacts=data[data["category"]=="contacts"][["text","link"]].replace({no.nan:None}).values
-    skills=data[data["category"]=="skills"]["text"].values
-    projects=data[data["category"]=="projects"][["text","link"]].replace({no.nan:None}).values
-    education=data[data["category"]=="education"]["text"].values
-    achivments=data[data["category"]=="achivments"]["text"].values
-    facts=data[data["category"]=="facts"]["text"].values
-    return render_template("index.html",name=name,photo=photo,position=position,contscts=contacts,skills=skills,projects=projects,education=education,achivments=achivments,facts=facts)
+    data = pd.read_csv("Ріта.csv")
+    photo = "5463383737397013754.jpg"
+    name = data[data["category"] == "name"]["text"].values[0]
+    position = data[data["category"] == "position"]["text"].values[0]
+    contacts = data[data["category"] == "contacts"][["text", "link"]].replace({np.nan: None}).values
+    summary = data[data["category"] == "summary"]["text"].values[0] # ⬅️⬅️⬅️⬅️⬅️
+    skills = data[data["category"] == "skills"]["text"].values
+    projects = data[data["category"] == "projects"][["text", "link"]].replace({np.nan: None}).values
+    education = data[data["category"] == "education"]["text"].values
+    achievements = data[data["category"] == "achievements"]["text"].values
+    facts = data[data["category"] == "facts"]["text"].values
 
-folder=os.getcwd()
-app=Flask(__name__,template_folder=folder,static_folder=folder)
-app.add_url_rule("/","index",index)
+    return render_template("index.html", name=name, photo=photo,
+                           position=position, summary=summary,
+                           contacts=contacts, skills=skills,
+                           projects=projects,
+                           education=education, achievements=achievements,
+                           facts=facts)
+
+
+folder = os.getcwd() # запам'ятали поточну робочу папку
+app = Flask(__name__, template_folder=folder, static_folder=folder)
+app.add_url_rule("/", "index", index)
+
 app.run()
-
-
